ae06c15a5124d901b616db69c8301b4ec00081a2
Jonas Enlund <jonas.enlund@gmail.com>
2022-12-01 09:32:39 +0200

    day 1

---
+++ 2022/day01.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+
+const inputs = fs.readFileSync("day01.txt", "utf-8").split("\n\n");
+
+let sums = [];
+
+for (const input of inputs) {
+  const sum = input
+    .split("\n")
+    .map((n) => parseInt(n, 10))
+    .reduce((aggr, n) => aggr + n, 0);
+
+  sums.push(sum);
+}
+
+sums.sort((a, b) => b - a);
+
+console.log("Part 1:", sums[0]);
+console.log("Part 2:", sums[0] + sums[1] + sums[2]);
